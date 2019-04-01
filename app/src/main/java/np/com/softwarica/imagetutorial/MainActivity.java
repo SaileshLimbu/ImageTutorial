521c83c2b903323049ac3b1895858e27cce039e5
sailesh limbu <saileshlimbu04@gmail.com>
2019-04-01 10:50:10 +0545

    Added Images and Code

--- app/src/main/java/np/com/softwarica/imagetutorial/MainActivity.java
+++ app/src/main/java/np/com/softwarica/imagetutorial/MainActivity.java
@@ -1,11 +1,12 @@
 package np.com.softwarica.imagetutorial;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private RadioButton rdRajesh, rdShiva, rdBhuwan;
     private ImageView imgDais;
@@ -19,5 +20,25 @@ public class MainActivity extends AppCompatActivity {
         rdShiva = findViewById(R.id.rdShiva);
         rdBhuwan = findViewById(R.id.rdBhuwan);
         imgDais = findViewById(R.id.imgDais);
+
+        rdRajesh.setOnClickListener(this);
+        rdShiva.setOnClickListener(this);
+        rdBhuwan.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.rdRajesh:
+                imgDais.setImageResource(R.drawable.rajesh);
+                break;
+            case R.id.rdShiva:
+                imgDais.setImageResource(R.drawable.shiva);
+                break;
+            case R.id.rdBhuwan:
+                imgDais.setImageResource(R.drawable.bhuwan);
+                break;
+
+        }
     }
 }
